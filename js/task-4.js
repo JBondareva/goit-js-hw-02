f9a9af07018f058aa75a21cc653dcdb9a3f4335c
JBondareva <juliabond1209@gmail.com>
2023-11-19 21:31:41 -0600

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,15 @@
-function getShippingCost(country) {
-  let shippingCost;
-  switch (country.toLowerCase()) {
-    case 'china':
-      shippingCost = 100;
-      break;
-    case 'chile':
-      shippingCost = 250;
-      break;
-    case 'australia':
-      shippingCost = 170;
-      break;
-    case 'jamaica':
-      shippingCost = 120;
-      break;
-    default:
-      return `Sorry, there is no delivery to your country`;
-  }
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
 
-  return `Shipping to ${country} will cost ${shippingCost} credits`;
+  if (totalPrice > customerCredits) {
+    return 'Insufficient funds!';
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
-console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
